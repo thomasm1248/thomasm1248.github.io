4031e93f2c2a59aac2c1d85cdde79d9a442547b5
thomasm1248 <thomasm1248@gmail.com>
2026-02-21 07:15:59 -0700

    Update tame.js

--- tame.js
+++ tame.js
@@ -17,14 +17,18 @@
  * write safer code that fails fast and is easy to debug.
  *
  * Including:
+ *
  * <script src="tame.js"></script>
  *
  * Examples of use:
  *
- * d.assert(foo === 0, `foo was ${foo} instead of zero.`);
- * d.shape({ a: 'string', b: 'number' }, object); // enforce shape
- * d.log('snapshot of foo', foo); // returns last parameter
- * d.freeze(obj); // prevent modification (recursively)
+ * t.assert(foo === 0, `foo was ${foo} instead of zero.`);
+ * t.shape({ a: {c: 'string'}, b: ['number'] }, object); // enforce shape
+ * t.log('snapshot of foo', foo); // returns last parameter
+ * t.freeze(obj); // prevent modification (recursively)
+ * t.tag('modified in bar()', obj);
+ * t.transferTags(obj, obj2);
+ * t.log('obj2', t.tags(obj2))
  *
  * Rules to follow in your code:
  *
@@ -39,41 +43,81 @@
  */
 
 var t = {
+    enabled: true,
+
     assert(condition, message) {
+        if(!t.enabled) return;
         console.assert(condition, message);
     },
 
     shape(spec, value, path) {
-        if(path === undefined) path = '';
+        if(!t.enabled) return value;
+        if(path === undefined) path = 'obj';
         if(typeof spec === 'string') {
             if(typeof value !== spec) {
                 t.log(
                     'Expected type:', spec,
                     '\nActual value:', value);
-                throw new Error(`expected '${spec}', got '${typeof value}'`);
+                throw new Error(`expected '${spec}' from ${path}, got '${typeof value}'`);
             }
         } else if(typeof spec === 'object') {
-            if(typeof value !== 'object') {
-                t.log(
-                    'Expected shape:', spec,
-                    '\nActual value:', value);
-                throw new Error(`expected 'object', got '${typeof value}'`);
+            if(spec.length === undefined) {
+                // Object
+                if(typeof value !== 'object') {
+                    t.log(
+                        'Expected shape:', spec,
+                        '\nActual value:', value);
+                    throw new Error(`expected 'object' from ${path}, got '${typeof value}'`);
+                }
+                for(const key in spec)
+                    t.shape(spec[key], value[key], path + '.' + key);
+            } else {
+                // List
+                let listSpec = spec[0];
+                for(var i = 0; i < value.length; i++)
+                    t.shape(listSpec, value[i], `${path}[${i}]`);
             }
-            for(const key in spec)
-                t.shape(spec[key], value[key]);
         }
         else {
             throw new Error("'spec' must be of type 'string' or 'object'");
         }
+        return value;
     },
 
     // Same as console.log, but objects are copied
     log() {
+        if(!t.enabled) return arguments[arguments.length-1];
         let clones = [];
         for(let a in arguments)
             clones.push(structuredClone(arguments[a]));
         console.log(...clones);
         return arguments[arguments.length-1];
+    },
+
+    tag(tag, obj) {
+        t.shape('string', tag);
+        t.shape({}, obj);
+        if(obj.debugTagHistory === undefined)
+            obj.debugTagHistory = [];
+        obj.debugTagHistory.push(tag);
+        return obj;
+    },
+
+    transferTags(from, to) {
+        t.shape({}, from);
+        t.shape({}, to);
+        if(from.debugTagHistory === undefined)
+            return;
+        if(to.debugTagHistory === undefined)
+            to.debugTagHistory = [];
+        to.debugTagHistory.push(...from.debugTagHistory);
+        return to;
+    },
+
+    tags(obj) {
+        t.shape({}, obj);
+        if(obj.debugTagHistory === undefined) return '<no tags>';
+        return obj.debugTagHistory.join('\n');
     },
 
     // Recursively freeze objects and their sub-objects
