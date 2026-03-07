72a427da51e2e08e5a2e85b7bbc64e9bf60fe9d9
thomasm1248 <thomasm1248@gmail.com>
2026-03-07 08:45:23 -0700

    Update chatbot

--- chat-bot/brainCode.js
+++ chat-bot/brainCode.js
@@ -11,6 +11,8 @@ start: WHO AWAKENS ME FROM MY SLUMBER?!
 whoAreYou: Who are you, and from whence do you come to bother me?!!
 .visitor: Just a visitor stopping by...
 ->doYouKnowMe
+.thomas: Thomas
+->whatToDoMaster
 
 doYouKnowMe: Peon of the dust, DO YOU KNOW WHO I AM?!!!!!
 .yes: Yeah, I've already met you
@@ -18,8 +20,16 @@ doYouKnowMe: Peon of the dust, DO YOU KNOW WHO I AM?!!!!!
 .no: No, who are you?
 ->whoIAm
 
-ohOkay: Oh, okay.
-.bye: Bye
+ohOkay: Oh, okay. How can I be of service?
+.somethingCool: Show me something cool
+->https://omni.vi/demos/diffuse
+->https://tokipona.org/
+->https://www.window-swap.com/
+->https://onio.club/
+->https://melonland.net/
+.master: Take me to your master
+->mailto:thomasm1248@gmail.com
+.nothing: No need. I'm done
 
 whoIAm: I AM THE LORD OF THIS GREAT SQUARE! I own every rectangle in this province and I rule from the left edge all the way to the right edge, and from the top edge all the way to the bottom edge! BOW BEFORE ME PEASANT!!! It is by my good-will that you are granted passage through this place.
 .okay: Okay???
@@ -32,4 +42,7 @@ waterReminder: MWAAAHAAHAHAHAHAHAAAAA!!! You have gone long enough without the l
 
 ohIThoughtYouWereDehydrated: Oh, I could have sworn I felt your soul slowly diminishing. It must have been someone else. I'LL BE BACK!!!!!!
 .okay: Okay, whatever
+
+whatToDoMaster: Master, what shall I do for thee?
+.nothing: Nothing yet
 `}));
