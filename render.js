b8a8980bd453d7870a7a9387e49c59f157e707a9
thomasm1248 <thomasm1248@gmail.com>
2026-03-09 22:00:22 -0600

    Fix darkmode schedule

--- render.js
+++ render.js
@@ -141,8 +141,8 @@ fetch('content.txt')
     // date => bool
     // Decides wether a given time counts as night
     // for the purposes of darkmode on this website
-    date.getHours() < 6 ||
-    date.getHours() > 6;
+    date.getHours() < 6 || // < 6:00 am
+    date.getHours() > 18;  // > 6:59 pm
 
   const setDarkMode = darkModeEnabled => {
     const body = document.getElementsByTagName('body')[0];
