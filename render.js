cc8fab9024b44f4450e88f5ef70dc0550ff26470
thomasm1248 <thomasm1248@gmail.com>
2026-03-09 21:54:11 -0600

    Add automatic dark mode

--- render.js
+++ render.js
@@ -134,3 +134,31 @@ fetch('content.txt')
     t.log('Error: ' + error);
   });
 
+// Darkmode Management
+
+(() => {
+  const isNightTime = date =>
+    // date => bool
+    // Decides wether a given time counts as night
+    // for the purposes of darkmode on this website
+    date.getHours() < 6 ||
+    date.getHours() > 6;
+
+  const setDarkMode = darkModeEnabled => {
+    const body = document.getElementsByTagName('body')[0];
+    if(darkModeEnabled)
+      body.classList.add('night-time');
+    else
+      body.classList.remove('night-time');
+  };
+
+  const checkForDarkMode = () =>
+    setDarkMode(
+      isNightTime(
+        new Date()));
+
+  checkForDarkMode();
+
+  setInterval(checkForDarkMode, 60000);
+
+})();
