18c1338fe7914ff911318622f7c804c98805eb9d
thomasm1248 <thomasm1248@gmail.com>
2026-03-16 17:19:19 -0600

    Make pages darkmode too

--- render.js
+++ render.js
@@ -133,32 +133,3 @@ fetch('content.txt')
   .catch(error => {
     t.log('Error: ' + error);
   });
-
-// Darkmode Management
-
-(() => {
-  const isNightTime = date =>
-    // date => bool
-    // Decides wether a given time counts as night
-    // for the purposes of darkmode on this website
-    date.getHours() < 6 || // < 6:00 am
-    date.getHours() > 18;  // > 6:59 pm
-
-  const setDarkMode = darkModeEnabled => {
-    const body = document.getElementsByTagName('body')[0];
-    if(darkModeEnabled)
-      body.classList.add('night-time');
-    else
-      body.classList.remove('night-time');
-  };
-
-  const checkForDarkMode = () =>
-    setDarkMode(
-      isNightTime(
-        new Date()));
-
-  checkForDarkMode();
-
-  setInterval(checkForDarkMode, 60000);
-
-})();
